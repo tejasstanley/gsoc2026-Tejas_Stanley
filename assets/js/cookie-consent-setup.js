59b11d53e79798fa705ebcefd146ef0574d80c87
tejasstanley <140648660+tejasstanley@users.noreply.github.com>
2026-05-06 19:47:54 +0000

    Deploying to gh-pages from @ tejasstanley/gsoc2026-Tejas_Stanley@7a13394830eecd3122dc0b62d71882680e0aed8b 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://alshedivat.github.io/al-folio/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://github.com/tejasstanley/gsoc2026-Tejas_Stanley/#contact">contact us</a>.'
               }
             ]
           }
